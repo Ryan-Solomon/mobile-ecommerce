c2ce126f59f496e534d83758320515c9cac096e1
Ryan <ryan@ryansolomon.io>
2021-06-01 06:16:44 -0600

    add product screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { ProductScreen } from './src/screens/ProductScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ProductScreen />
     </View>
   );
 }
